b4ccfc31117eedeb50f279733e01dd67859674fa
seanholden <seanholden1@gmail.com>
2013-10-12 18:57:47 +0100

    added gifts route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ CheersPal::Application.routes.draw do
 
   namespace :api do
     resources :login
+    resources :gifts
   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
