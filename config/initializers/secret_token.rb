0a012c0ea3c42a7f9d8e416e24583f6c6b77ff2e
seanholden <seanholden1@gmail.com>
2013-10-12 17:29:21 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CheersPal::Application.config.secret_key_base = '5be14ac7bbda8b400bfbca15a9f3a82d321ec2b0a81c3eb538d186703f58e89257ef2d930344e84a0ab9c750eb098d4bbfcd23f518ab917c9e5897ded6bb754b'
