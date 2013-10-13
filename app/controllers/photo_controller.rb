650d62c530dadfd0d1815fbd06a37ef50ddd361b
seanholden <seanholden1@gmail.com>
2013-10-13 04:48:57 +0100

    a

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -1,10 +1,14 @@
 class PhotoController < ApplicationController
   protect_from_forgery except: [:create]
-  
+
   def create
     puts "IMAGE:"
     puts params
+    # gift = Gift.find(params[:id])
+    # gift.update_attributes(claim_time:Time.now, charity:false)
+    # 
+    uploader = AvatarUploader.new
+    uploader.store!(params[:image])
     render json: {"endpoint" => "reached"}
-    # uploader = AvatarUploader.new
   end
 end
