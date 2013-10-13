8aaa8c9e7383748d78d2d5df6308594767b4b3fe
seanholden <seanholden1@gmail.com>
2013-10-13 04:17:52 +0100

    meh

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -2,6 +2,7 @@ class PhotoController < ApplicationController
   def create
     puts "IMAGE:"
     puts params
+    render json: {"endpoint" => "reached"}
     # uploader = AvatarUploader.new
   end
 end
